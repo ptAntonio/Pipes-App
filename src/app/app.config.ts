9c2aae6490b16b98efc123da1bedfde93bbbbac7
Antonio Perez <64748202+AntPerDev@users.noreply.github.com>
2025-08-28 08:03:22 +0200

    added change locale by buttons es, en, fr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,30 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+//locales registrados en la app para poder cambiar su idioma
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import { LocaleService } from './services/locale.service';
+
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeFr, 'fr');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    
+
+
+    // Registrar idiomas para cambiar el locale de la aplicación,
+    {
+      provide: LOCALE_ID, // esto se importa de @angular/core
+      // useValue: 'fr'
+      deps: [LocaleService],
+      useFactory: (localService: LocaleService) => localService.getLocale,
+    }
   ]
 };
--- src/app/pages/basic-component/basic-component.component.ts
+++ src/app/pages/basic-component/basic-component.component.ts
@@ -1,15 +1,41 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe, PercentPipe } from '@angular/common';
+import { Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+import { AvailableLocale, LocaleService } from '../../services/locale.service';
 
 @Component({
   selector: 'app-basic-component',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-component.component.html',
 })
 export default class BasicComponentComponent {
 
+  localeService = inject(LocaleService);
+  currentLocale = signal(inject(LOCALE_ID));
+
   nameLower = signal('Antonio');
   nameUpper = signal('ANTONIO');
-  fullName  = signal('anTOniO pEreZ');
+  fullName = signal('anTOniO pEreZ');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanUp) => {
+
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+
+      onCleanUp(() => {
+        clearInterval(interval)
+      });
+
+    }, 1000);
+  })
+
+  changeLocale(locale: AvailableLocale) {
+    console.log({ locale });
+    this.localeService.changeLocale(locale);
+  }
 
 }
+
+
